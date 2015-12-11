6dd994acfb0faa2d7f5aa530148f8f5840369e7d
Laurie Koudstaal <laurie.koudstaal@fastmail.com>
2015-12-12 07:57:12 +1100

    Fixed up broken merge

--- init.el
+++ init.el
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-<<<<<<< HEAD
-=======
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -24,7 +18,6 @@
  ;; If there is more than one, they won't work right.
  )
 
->>>>>>> origin/master
 (server-start)
 (add-to-list 'exec-path "/usr/local/bin/")
 (add-to-list 'exec-path "/usr/local/mysql/bin")
@@ -118,24 +111,6 @@
 (setq mac-command-modifier 'control)
 (let ((default-directory "/usr/local/share/emacs/site-lisp/"))
   (normal-top-level-add-subdirs-to-load-path))
-<<<<<<< HEAD
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (leuven)))
- '(package-selected-packages (quote (web-mode org org-journal magit))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-=======
-
-
->>>>>>> origin/master
 
 ;; BACKUPS
 (setq backup-directory-alist `(("." . "~/.saves")))

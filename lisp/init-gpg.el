74070f6efd3bc0c103c115f33e7e677340e52d57
Laurie Koudstaal <laurie.koudstaal@fastmail.com>
2017-02-18 18:03:11 +1100

    Added reminder

--- lisp/init-gpg.el
+++ lisp/init-gpg.el
@@ -1,4 +1,6 @@
+;; To setup on mac, make sure that you've installed gpg
+;; and pinentry-mac from homebrew
 (require 'epa-file)
-(custom-set-variables '(epg-gpg-program  "/usr/local/bin/gpg2"))
+(custom-set-variables '(epg-gpg-program  "/usr/local/bin/gpg"))
 (epa-file-enable)
 (provide 'init-gpg)

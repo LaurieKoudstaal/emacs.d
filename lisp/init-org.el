7747b4080377e5ea6ec117bca016a16c530a1187
Laurie Koudstaal <laurie.koudstaal@veeva.com>
2017-03-03 16:15:03 +1100

    Moved some functions to library

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -307,36 +307,9 @@ typical word processor."
 (global-set-key (kbd "C-c |") 'my-edit-dsv-as-orgtbl)
 
 
-;;; ID FOR NEW ITEMS 2
-;; MAKE AN ID THAT LOOKS A BIT HUMAN READABLE
-(defun lkk/generate-human-readable-id ()
-  (defconst con-list "BCDFGHJKLMNPQRSTVWXZ")
-  (defconst vow-list "AEIOUY")
-  (defconst num-list "0123456789")
-  (defvar ret-id "")
-  (setq ret-id (concat (string (elt con-list (random 20)))
-		       (string (elt vow-list (random 6)))
-		       (string (elt con-list (random 20)))
-		       (string (elt num-list (random 10)))))
-  ret-id)
-
-
-;;; ID FOR NEW ITEMS
-;; MAKE 4-BASE36 ID
-(defun lkk/base-36 (n)
-  (defconst char-list "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-  (defvar encoding "")
-  (setq encoding "")
-  (while (> n 0)
-    (setq encoding (concat encoding (string (elt char-list (random 36)))))
-    (setq n (- n 1)))
-  encoding)
-
 (defun lkk/org-id-get-create ()
   (interactive)
   (org-set-property "CATEGORY" (lkk/generate-human-readable-id)))
-
-(add-hook 'org-capture-prepare-finalize-hook 'lkk/org-id-get-create)
 
 ;; ORG MODE: ADD CUSTOMER
 (defun lkk/org-add-customer (arg)

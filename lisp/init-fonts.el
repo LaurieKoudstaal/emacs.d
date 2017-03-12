83fabfc66cbf9477f961980ad5111130a1ca61a6
Laurie Koudstaal <laurie.koudstaal@veeva.com>
2017-03-12 22:19:36 +1100

    Fixed bug in font settings

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,4 +1,4 @@
-(add-to-list 'default-frame-alist '(font . "Menlo" ))
-(set-face-attribute 'default t :font "Menlo" )
-;(set-face-font 'fixed-pitch "Menlo")
+(add-to-list 'default-frame-alist '(font . "Menlo-12" ))
+(set-face-attribute 'default nil :font "Menlo-12" )
+;;(set-face-font 'fixed-pitch "Menlo")
 (provide 'init-fonts)

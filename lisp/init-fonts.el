b0ad9a0d002cfc83935246c478831a882e771156
Laurie Koudstaal <laurie.koudstaal@veeva.com>
2017-07-29 10:32:34 +1000

    Using fixed-pitch for fonts

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,4 +1,5 @@
 (add-to-list 'default-frame-alist '(font . "Input Sans-11" ))
 (set-face-attribute 'default nil :font "Input Sans-11" )
 (set-face-font 'fixed-pitch "Input Mono-11")
+(set-face-attribute 'org-table nil :inherit 'fixed-pitch)
 (provide 'init-fonts)

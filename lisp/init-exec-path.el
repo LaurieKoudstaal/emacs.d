c865e6923333087135abffd7f5cdab3cd669e221
Laurie Koudstaal <laurie.koudstaal@veeva.com>
2017-09-02 17:00:05 +1000

    Fixed init paths for windows

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -2,9 +2,8 @@
 
 ;; EXEC PATH FROM SHELL
 (when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
-
-;; COPY PYTHON PATH FROM SHELL
-(exec-path-from-shell-copy-env "PYTHONPATH")
+  (exec-path-from-shell-initialize)
+  ;; COPY PYTHON PATH FROM SHELL
+  (exec-path-from-shell-copy-env "PYTHONPATH"))
 
 (provide 'init-exec-path)

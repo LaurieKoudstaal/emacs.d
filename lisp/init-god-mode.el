d2f0b4288ff98b92701855393ec6e7dc425d2ea3
Laurie Koudstaal <laurie.koudstaal@veeva.com>
2017-10-05 23:32:07 +1100

    God-mode uses M-g

--- lisp/init-god-mode.el
+++ lisp/init-god-mode.el
@@ -1,6 +1,6 @@
 ;; GOD MODE
 (require-package 'god-mode)
-(global-set-key (kbd "C-c g") 'god-mode-all)
+(global-set-key (kbd "M-g") 'god-mode-all)
 
 (defun my-update-cursor ()
   (setq cursor-type (if (or god-local-mode buffer-read-only)

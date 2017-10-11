9ed5f425a2ae826d80d833240071759641611c9e
Laurie Koudstaal <laurie.koudstaal@veeva.com>
2017-10-11 18:25:26 +1100

    God mode sets the cursor color for modal clarity

--- lisp/init-god-mode.el
+++ lisp/init-god-mode.el
@@ -3,9 +3,11 @@
 (global-set-key (kbd "M-g") 'god-mode-all)
 
 (defun my-update-cursor ()
-  (setq cursor-type (if (or god-local-mode buffer-read-only)
-                        'box
-                      'bar)))
+  (cond ((or (bound-and-true-p god-mode)
+	     (bound-and-true-p god-global-mode)
+	     (bound-and-true-p god-local-mode))
+	 (set-cursor-color "lime green"))
+	(t (set-cursor-color "dark orange"))))
 
 (add-hook 'god-mode-enabled-hook 'my-update-cursor)
 (add-hook 'god-mode-disabled-hook 'my-update-cursor)

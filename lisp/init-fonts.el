9bc9547bb9df3924f6b7ff034d482fffc0e90774
Laurie Koudstaal <laurie.koudstaal@veeva.com>
2017-10-11 18:24:38 +1100

    Fonts now only uses variable pitch in some instances

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,4 +1,26 @@
-(add-to-list 'default-frame-alist '(font . "InputSans-11" ))
-(set-face-attribute 'default nil :font "InputSans-11" )
 (set-face-font 'fixed-pitch "InputMono-11")
+(set-face-font 'variable-pitch "InputSans-12")
+
+;; FONTS
+;; -----
+;; Set variable-pitch font using customize-face variable-pitch
+;; Set the fonts to format correctly for specific modes. Default is set for fixed
+;; so we only need to have the exceptions
+(defun set-buffer-variable-pitch ()
+  (interactive)
+  (variable-pitch-mode t)
+  (setq line-spacing 3)
+  (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+;  (set-face-attribute 'org-link nil :inherit 'fixed-pitch)
+;  (set-face-attribute 'org-code nil :inherit 'fixed-pitch)
+;  (set-face-attribute 'org-block nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-date nil :inherit 'fixed-pitch)
+ ; (set-face-attribute 'org-special-keyword nil :inherit 'fixed-pitch)
+  )
+
+(add-hook 'org-mode-hook 'set-buffer-variable-pitch)
+(add-hook 'markdown-mode-hook 'set-buffer-variable-pitch)
+(add-hook 'Info-mode-hook 'set-buffer-variable-pitch)
+
+
 (provide 'init-fonts)

8590dc12cb40ba2101369189feb8061ebbd5d946
Laurie Koudstaal <laurie.koudstaal@fastmail.com>
2017-10-24 22:32:31 +0000

    Added timeout for escape for ssh that sends ESC

--- lisp/init-god-mode.el
+++ lisp/init-god-mode.el
@@ -1,6 +1,11 @@
 ;; GOD MODE
 (require-package 'god-mode)
-(global-set-key (kbd "M-g") 'god-mode-all)
+(require-package 'key-chord)
+(key-chord-mode 1)
+(setq mac-command-modifier 'super)
+(key-chord-define-global "gg" 'god-mode-all)
+(global-set-key (kbd "M-]") 'god-mode-all)
+(global-set-key (kbd "<escape>") 'god-mode-all)
 
 (defun my-update-cursor ()
   (setq cursor-type (if (or god-local-mode buffer-read-only)
@@ -14,5 +19,30 @@
 
 (add-to-list 'god-exempt-major-modes 'org-agenda-mode)
 
+(defvar personal/fast-keyseq-timeout 200)
+
+(defun personal/-tty-ESC-filter (map)
+  (if (and (equal (this-single-command-keys) [?\e])
+           (sit-for (/ personal/fast-keyseq-timeout 1000.0)))
+      [escape] map))
+
+(defun personal/-lookup-key (map key)
+  (catch 'found
+    (map-keymap (lambda (k b) (if (equal key k) (throw 'found b))) map)))
+
+(defun personal/catch-tty-ESC ()
+  "Setup key mappings of current terminal to turn a tty's ESC into `escape'."
+  (when (memq (terminal-live-p (frame-terminal)) '(t pc))
+    (let ((esc-binding (personal/-lookup-key input-decode-map ?\e)))
+      (define-key input-decode-map
+        [?\e] `(menu-item "" ,esc-binding :filter personal/-tty-ESC-filter)))))
+
+(personal/catch-tty-ESC)
+
+(define-key god-local-mode-map (kbd ".") 'repeat)
+(define-key god-local-mode-map (kbd "i") 'god-mode-all)
+
 (provide 'init-god-mode)
+
+
 

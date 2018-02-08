8c34ab0bd69d9333bdb6c5b75d9e721e485d24ec
Laurie Koudstaal <lauriekoudstaal@fastmail.com>
2018-02-08 22:49:00 +1100

    Added windows specific code

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 
 ;;; Detect system-type
 (defconst *is-a-mac* (eq system-type 'darwin))
-
+(defconst *is-win* (eq system-type 'windows-nt))
 
 ;;; Set up melpa
 (require 'package)
@@ -9,7 +9,7 @@
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired  
-  (add-to-list 'package-archives (cons "marmalade" (concat proto "://marmalade-repo.org/packages/")) t)
+  ;; (add-to-list 'package-archives (cons "marmalade" (concat proto "://marmalade-repo.org/packages/")) t)
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
@@ -40,9 +40,18 @@ re-downloaded in order to locate PACKAGE."
 (tool-bar-mode -1)
 
 
+;;; Fonts
+(when *is-win*
+  (add-to-list 'default-frame-alist '(font . "Consolas Bold-10" ))
+  (set-face-attribute 'default t :font "Consolas Bold-10" ))
+
+
 ;;; Keyboard Remappings
-(setq mac-command-modifier 'control)
-(setq mac-option-modifier 'meta)
+(when *is-a-mac*
+  (setq mac-command-modifier 'control)
+  (setq mac-option-modifier 'meta))
+(when *is-win*
+  (setq w32-apps-modifier 'control))
 
 
 
@@ -51,13 +60,46 @@ re-downloaded in order to locate PACKAGE."
 (require 'better-defaults)
 
 
+
+;;; Enable theme
+(require-package 'zenburn-theme)
+(require 'zenburn-theme)
+(load-theme 'zenburn)
+
+
+
+;;; God mode
+(require-package 'god-mode)
+(require 'god-mode)
+(global-set-key (kbd "<escape>") 'god-local-mode)
+(defun my-update-cursor ()
+  (setq cursor-type (if (or god-local-mode buffer-read-only)
+                        'box
+                      'bar)))
+
+(add-hook 'god-mode-enabled-hook 'c/god-mode-update-cursor)
+(add-hook 'god-mode-disabled-hook 'c/god-mode-update-cursor)
+
+(defun c/god-mode-update-cursor ()
+  (let ((limited-colors-p (> 257 (length (defined-colors)))))
+    (cond (god-local-mode (progn
+                            (set-face-background 'mode-line (if limited-colors-p "white" "#e9e2cb"))
+                            (set-face-background 'mode-line-inactive (if limited-colors-p "white" "#e9e2cb"))))
+          (t (progn
+               (set-face-background 'mode-line (if limited-colors-p "black" "#0a2832"))
+               (set-face-background 'mode-line-inactive (if limited-colors-p "black" "#0a2832")))))))
+
+
 ;; Custom settings
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (better-defaults))))
+ '(custom-safe-themes
+   (quote
+    ("599f1561d84229e02807c952919cd9b0fbaa97ace123851df84806b067666332" default)))
+ '(package-selected-packages (quote (god-mode better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

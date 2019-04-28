d4d437b37c81c9f6fd49fbdb749c32d06653101c
Laurie Koudstaal <laurie.koudstaal@veeva.com>
2019-04-28 12:01:46 +1000

    added birbo theme

---
+++ themes/birbo.el
@@ -0,0 +1,11 @@
+(deftheme birbo
+  "Created 2019-02-15.")
+
+(custom-theme-set-faces
+ 'birbo
+ '(widget-field ((t (:background "#50898B" :foreground "#DCDCCC"))))
+ '(cursor ((t (:background "#B0D9DB" :foreground "#DCDCCC"))))
+ '(default ((t (:inherit nil :stipple nil :background "#00393B" :foreground "#DCDCCC"))))
+ '(fringe ((t (:background "#10494B" :foreground "#DCDCCC")))))
+
+(provide-theme 'birbo)
